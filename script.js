074003a64a9f5861f91056967a2c243d699acfba
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-04-17 15:58:45 +0100

    add some styling and date of the current week

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
 const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-const employees = ["Hamza", "Badr", "Ilyass", "Yassine", "Younes"];
+const employees = ["Badr", "Hamza", "Ilyass", "Yassine", "Youness"];
 
-function generateSchedule() {
-  const shuffledDays = daysOfWeek.slice().sort(() => Math.random() - 0.5); // Shuffle days
+function getWeekDates() {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const startDate = new Date(today.setDate(today.getDate() - dayOfWeek));
+
+  const weekDates = [];
+  for (let i = 0; i < 5; i++) {
+    const currentDate = new Date(startDate);
+    currentDate.setDate(startDate.getDate() + i);
+    weekDates.push(currentDate);
+  }
+  return weekDates;
+}
+
+function generateSchedule(weekDates) {
+  const shuffledDays = daysOfWeek.slice().sort(() => Math.random() - 0.5);
   const schedule = {};
 
   employees.forEach((employee, index) => {
     schedule[employee] = shuffledDays[index];
   });
 
-  displaySchedule(schedule);
+  displaySchedule(schedule, weekDates);
 }
 
-function displaySchedule(schedule) {
+function displaySchedule(schedule, weekDates) {
   const employeeList = document.getElementById("employee-list");
-  employeeList.innerHTML = ""; // Clear previous schedule
-
+  employeeList.innerHTML = "";
+  let i = 0;
   for (const employee in schedule) {
+    const date = weekDates[i];
     const card = document.createElement("div");
     card.classList.add("col-sm-4", "card");
 
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
+    const dayClass = schedule[employee].toLowerCase();
+    cardBody.classList.add(dayClass);
+
     cardBody.innerHTML = `
        <h5 class="card-title">${employee}</h5>
-       <p class="card-text">Remote Day: ${schedule[employee]}</p>
+       <span class="card-text">Remote Day : ${schedule[employee]}</span>
+       <span class='badge badge-dark' >${date.toLocaleDateString()}</span>
     `;
 
     card.appendChild(cardBody);
     employeeList.appendChild(card);
+    i++;
   }
 }
 
-// Event Listener
 document
   .getElementById("generate-schedule")
-  .addEventListener("click", generateSchedule);
+  .addEventListener("click", () => generateSchedule(getWeekDates()));
