37bf06681e0ff8de372c98c1977f988388c075b4
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-04-18 16:26:03 +0100

    fix remote date value and add random worker generator

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function getWeekDates() {
   const startDate = new Date(today.setDate(today.getDate() - dayOfWeek));
 
   const weekDates = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     const currentDate = new Date(startDate);
     currentDate.setDate(startDate.getDate() + i);
     weekDates.push(currentDate);
   }
-  return weekDates;
+  return weekDates.map((date) => ({
+    date,
+    day: daysOfWeek[date.getDay() - 1],
+  }));
 }
 
 function generateSchedule(weekDates) {
@@ -20,7 +23,10 @@ function generateSchedule(weekDates) {
   const schedule = {};
 
   employees.forEach((employee, index) => {
-    schedule[employee] = shuffledDays[index];
+    schedule[employee] = {
+      day: shuffledDays[index],
+      date: weekDates.find((dd) => shuffledDays[index] === dd.day)?.date,
+    };
   });
 
   displaySchedule(schedule, weekDates);
@@ -29,9 +35,7 @@ function generateSchedule(weekDates) {
 function displaySchedule(schedule, weekDates) {
   const employeeList = document.getElementById("employee-list");
   employeeList.innerHTML = "";
-  let i = 0;
   for (const employee in schedule) {
-    const date = weekDates[i];
     const card = document.createElement("div");
     card.classList.add("col-sm-4", "card");
     card.classList.add("m-2", "card");
@@ -39,21 +43,25 @@ function displaySchedule(schedule, weekDates) {
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
-    const dayClass = schedule[employee].toLowerCase();
+    const dayClass = schedule[employee].day.toLowerCase();
     cardBody.classList.add(dayClass);
 
     cardBody.innerHTML = `
        <h5 class="card-title">${employee}</h5>
-       <span class="card-text">Remote Day : ${schedule[employee]}</span>
-       <span class='badge badge-dark' >${date.toLocaleDateString()}</span>
+       <span class="card-text">Remote Day : ${schedule[employee]?.day}</span>
+       <span class='badge badge-dark' >${schedule[employee]?.date.toLocaleDateString()}</span>
     `;
 
     card.appendChild(cardBody);
     employeeList.appendChild(card);
-    i++;
   }
 }
 
 document
   .getElementById("generate-schedule")
   .addEventListener("click", () => generateSchedule(getWeekDates()));
+
+document.getElementById("random-worker").addEventListener("click", () => {
+  document.getElementById("worker-view").innerHTML =
+    `<strong>${employees[Math.floor(Math.random() * 5)]}</string>`;
+});
